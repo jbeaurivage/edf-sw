93d1905854f114bfb4074692ea48d4bd1683debf
Justin Beaurivage <code@beaurivage.io>
2025-11-09 14:01:39 +0100

    Benchmark schedule operation
    Signed-off-by: Justin Beaurivage <code@beaurivage.io>

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,19 @@ use atsamd_hal::pac::{Interrupt, NVIC, Peripherals, interrupt};
 use atsamd_hal::prelude::InterruptDrivenTimer;
 use atsamd_hal::timer::TimerCounter;
 use atsamd_hal::{self as hal};
+
 use cortex_m::asm;
+use cortex_m::peripheral::DWT;
 use cortex_m::peripheral::scb::SystemHandler;
+
 use fugit::ExtU32;
 use hal::rtc::rtic::rtc_clock;
-use {defmt_rtt as _, panic_probe as _};
 
 use edf_sw::Deadline;
 use edf_sw::scheduler::Scheduler;
 use edf_sw::task::Task;
+
+use {defmt_rtt as _, panic_probe as _};
 
 hal::rtc_monotonic!(Mono, rtc_clock::Clock32k);
 
@@ -47,19 +51,32 @@ fn main() -> ! {
     let timer_clock = clocks.gclk0();
     let tc45 = &clocks.tc4_tc5(&timer_clock).unwrap();
 
+    // Enable cycle counter for measurements
+    DWT::unlock();
+    unsafe {
+        core.DCB.demcr.modify(|r| r | (1 << 24));
+    }
+
     SCHEDULER.init(&mut core.NVIC, &mut core.SCB);
 
     // Instantiate a timer object for the TC4 timer/counter
     let mut timer = TimerCounter::tc4_(tc45, peripherals.tc4, &mut peripherals.mclk);
     timer.start(100.millis());
-    timer.enable_interrupt();
+    // timer.enable_interrupt();
 
     core.SYST.set_reload(8_000_000 - 1);
     core.SYST.clear_current();
     core.SYST.enable_interrupt();
-    core.SYST.enable_counter();
+    // core.SYST.enable_counter();
 
-    SCHEDULER.schedule(Task::new(Deadline::millis(500), software_task));
+    for i in 0..=16 {
+        let deadline = Deadline::millis(i + 1);
+        reset_dwt();
+        SCHEDULER.schedule(Task::new(deadline, software_task));
+    }
+
+    reset_dwt();
+    SCHEDULER.schedule(Task::new(Deadline::micros(60), software_task));
 
     defmt::debug!("[IDLE START]");
     SCHEDULER.idle();
@@ -67,6 +84,7 @@ fn main() -> ! {
 
 #[cortex_m_rt::exception]
 fn SysTick() {
+    reset_dwt();
     SCHEDULER.schedule(Task::new(Deadline::millis(2), systick_task));
 }
 
@@ -81,7 +99,7 @@ fn software_task() {
     // Simulate blocking roughly for 2s with CPU running at 48 MHz
     asm::delay(96_000_000);
     defmt::info!("[TASK 0] Software task complete");
-    SCHEDULER.schedule(Task::new(Deadline::millis(1000), software_task));
+    // SCHEDULER.schedule(Task::new(Deadline::millis(1000), software_task));
 }
 
 fn systick_task() {
@@ -92,4 +110,10 @@ fn systick_task() {
 fn timer_task() {
     asm::delay(8_000);
     defmt::info!("[TASK 1] Timer task complete");
+}
+
+fn reset_dwt() {
+    let mut dwt = unsafe { cortex_m::peripheral::Peripherals::steal() }.DWT;
+    dwt.set_cycle_count(0);
+    dwt.enable_cycle_counter();
 }
